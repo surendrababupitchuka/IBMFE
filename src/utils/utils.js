156bbce9a55a6e4c0379d279476d639e0317be11
surendrababupitchuka <99398852+surendrababupitchuka@users.noreply.github.com>
2022-02-10 16:23:23 +0530

    Delete utils.js

--- src/utils/utils.js
+++
@@ -1,8 +0,0 @@
-import Cookies from 'universal-cookie';
-export const cookies = new Cookies();
-
-export const removeAllCookies = () =>{
-    cookies.remove("login");
-    cookies.remove("USR_VLE");
-    cookies.remove('RST_VL');
-}
